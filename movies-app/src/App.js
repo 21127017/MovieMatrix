4c576ef200415fb85532f143e7b743551b2d8b3b
Gia Bao <21127017@student.hcmus.edu.vn>
2024-06-23 13:44:17 +0700

    user log out

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,5 +1,5 @@
 import { Link, Outlet, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Alert from './components/Alert';
 
 function App() {
@@ -10,9 +10,41 @@ function App() {
   const navigate = useNavigate();
 
   const logOut = () => {
-    setJwtToken("");
+    const requestOptions = {
+      method: "GET",
+      credential: "include",
+    }
+
+    fetch(`/logout`, requestOptions)
+      .catch(error => {
+        console.log("erorr logging out", error);
+      })
+      .finally(() => {
+        setJwtToken("");
+      })
+
     navigate("/login")
   }
+
+  useEffect(() => {
+    if (jwtToken === "") {
+      const requestOptions = {
+        method: "GET",
+        credential: "include",
+      }
+
+      fetch( `/refresh`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.access_token) {
+            setJwtToken(data.access_token);
+          }
+        })
+        .catch(error => {
+          console.log("user is not logged in", error);
+        })
+    }
+  }, [jwtToken])
 
   return (
     <div className='container'>
