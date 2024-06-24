eec102e409eb45d2549da6c92a863159d4b5483e
Gia Bao <21127017@student.hcmus.edu.vn>
2024-06-24 16:36:25 +0700

    handler for GraphQL

--- movie-backend/cmd/api/handlers.go
+++ movie-backend/cmd/api/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"backend/internal/graph"
 	"backend/internal/models"
 	"encoding/json"
 	"errors"
@@ -349,4 +350,27 @@ func (app *application) AllMoviesByGenres(w http.ResponseWriter, r *http.Request
 	}
 
 	app.writeJSON(w, http.StatusAccepted, movies)
+}
+
+func (app *application) moviesGraphQL(w http.ResponseWriter, r *http.Request) {
+	movies, _ := app.DB.AllMovies()
+
+	q, _ := io.ReadAll(r.Body)
+	query := string(q)
+
+	g := graph.New(movies)
+
+	g.QueryString = query
+
+	resp, err := g.Query()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	j, _ := json.MarshalIndent(resp, "", "\t")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
 }
