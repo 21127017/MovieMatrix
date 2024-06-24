a36e0b15063750d7aa669e6806732701be1e2f4c
Gia Bao <21127017@student.hcmus.edu.vn>
2024-06-24 16:28:06 +0700

    method for GraphQL (list, search, get)

--- movie-backend/internal/graph/graphql.go
+++ movie-backend/internal/graph/graphql.go
@@ -2,6 +2,8 @@ package graph
 
 import (
 	"backend/internal/models"
+	"errors"
+	"strings"
 
 	"github.com/graphql-go/graphql"
 )
@@ -49,4 +51,81 @@ func New(movies []*models.Movie) *Graph {
 			},
 		},
 	)
+
+	var fields = graphql.Fields{
+
+		"list": &graphql.Field{
+			Type:        graphql.NewList(movieType),
+			Description: "Get all movies",
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				return movies, nil
+			},
+		},
+
+		"search": &graphql.Field{
+			Type:        graphql.NewList(movieType),
+			Description: "Search movies by title",
+			Args: graphql.FieldConfigArgument{
+				"titleContains": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				var theList []*models.Movie
+				search, ok := params.Args["titleContains"].(string)
+				if ok {
+					for _, currentMovie := range movies {
+						if strings.Contains(strings.ToLower(currentMovie.Title), strings.ToLower(search)) {
+							theList = append(theList, currentMovie)
+						}
+					}
+				}
+				return theList, nil
+			},
+		},
+
+		"get": &graphql.Field{
+			Type:        movieType,
+			Description: "Get movie by id",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				id, ok := p.Args["id"].(int)
+				if ok {
+					for _, movie := range movies {
+						if movie.ID == id {
+							return movie, nil
+						}
+					}
+				}
+				return nil, nil
+			},
+		},
+	}
+
+	return &Graph{
+		Movies:    movies,
+		fields:    fields,
+		movieType: movieType,
+	}
+}
+
+func (g *Graph) Query() (*graphql.Result, error) {
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: g.fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	params := graphql.Params{Schema: schema, RequestString: g.QueryString}
+	resp := graphql.Do(params)
+	if len(resp.Errors) > 0 {
+		return nil, errors.New("error executing query")
+	}
+
+	return resp, nil
 }
